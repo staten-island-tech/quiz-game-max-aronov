a8575b58c6f1e31d4e4242a42de969fc7d334846
Requias <max.orin05@yahoo.com>
2021-04-07 16:06:39 -0400

    Never gonna give you up

---
+++ source/scripts/quiz/questions.js
@@ -0,0 +1,121 @@
+const questionsCache = [{
+        question: "How many planets are there in the Solar System",
+        choices: ["9", "10", "8", "6"],
+        correct: 3,
+        difficulty: "effortless"
+    },
+    {
+        question: "What galaxy is the Solar System a part of",
+        choices: ["Milky Way", "Andromeda", "Whirpool", "Cigar"],
+        correct: 1,
+        difficulty: "effortless"
+    },
+    {
+        question: "How old is the universe",
+        choices: [
+            "14.2 billion years",
+            "10.9 billion years",
+            "13.8 billion years",
+            "18.8 billion years"
+        ],
+        correct: 3,
+        difficulty: "effortless"
+    },
+    {
+        question: "What is a supernova",
+        choices: [
+            "An imploded planet",
+            "A dead star's explosion",
+            "A splitting galaxy",
+            "Another name for a gamma ray burst"
+        ],
+        correct: 2,
+        difficulty: "easy"
+    },
+    {
+        question: "What is a black hole",
+        choices: [
+            "A region in space-time of immense gravity",
+            "An immensly large planet",
+            "An exploding star",
+            "An imploding galaxy"
+        ],
+        correct: 1,
+        difficulty: "easy"
+    },
+    {
+        question: "How is a black hole created",
+        choices: [
+            "Matter gets squeezed to an immensly small size",
+            "A star swallows another star",
+            "Two planets collide",
+            "A galaxy collides with another galaxy"
+        ],
+        correct: 1,
+        difficulty: "easy"
+    },
+    {
+        question: "What type of galaxy is the Milky Way",
+        choices: ["Elliptical", "Irregular", "Spiral", "Lenticular"],
+        correct: 3,
+        difficulty: "medium"
+    },
+    {
+        question: "What lies in the center of most galaxies",
+        choices: [
+            "A Hypergiant Star",
+            "A toroidal planet",
+            "Nothing",
+            "A black hole"
+        ],
+        correct: 4,
+        difficulty: "medium"
+    },
+    {
+        question: "What star is predicted to go supernova sometime in the next 100,000 years",
+        choices: ["Arcturus", "UY Scuti", "Pistol Star", "Betelgeuse"],
+        correct: 2,
+        difficulty: "medium"
+    },
+    {
+        question: "How much of the universe is composed of plasma",
+        choices: ["10%", "50%", "0.24%", "99.9%"],
+        correct: 3,
+        difficulty: "hard"
+    },
+    {
+        question: "How many comets are in the Solar System",
+        choices: ["100,000", "1 million", "1 billion", "1 trillion"],
+        correct: 4,
+        difficulty: "hard"
+    },
+    {
+        question: "What star is predicted to go supernova sometime in the next 100,000 years",
+        choices: ["Arcturus", "UY Scuti", "Pistol Star", "Betelgeuse"],
+        correct: 2,
+        difficulty: "hard"
+    },
+    {
+        question: "How massive is the black hole TON 618",
+        choices: ["3.8×10¹¹", "12.2×10⁶", "9.89×10¹⁴", "6.6×10¹⁰"],
+        correct: 4,
+        difficulty: "insane"
+    },
+    {
+        question: "During which period of time during the birth of the universe were subatomic particles unable to form",
+        choices: [
+            "Planck Epoch",
+            "Grand Unification Epoch",
+            "Electroweak Epoch",
+            "Inflationary Epoch"
+        ],
+        correct: 1,
+        difficulty: "insane"
+    },
+    {
+        question: "How long did the Planck Epoch last",
+        choices: ["10⁻²¹", "10⁻⁴³", "10⁻³⁷", "10⁻⁶⁵"],
+        correct: 2,
+        difficulty: "insane"
+    }
+];
